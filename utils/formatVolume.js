458dd1a44e9e0c05528135891fad681647c5e026
knickerboxker-sudo <knickerboxker@gmail.com>
2026-02-14 21:19:08 -0500

    Delete utils directory

--- utils/formatVolume.js
+++
@@ -1,9 +0,0 @@
-export const formatVolume = (value) => {
-  const num = Number(value ?? 0);
-  return Number.isFinite(num)
-    ? new Intl.NumberFormat("en-US", {
-        notation: "compact",
-        maximumFractionDigits: 2,
-      }).format(num)
-    : "0";
-};
